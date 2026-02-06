63bbb5a681c78aab3e04d5e400c7da683926a183
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-02-06 09:33:34 -0500

    Third block layout completed.

---
+++ BlockLayout/ContentViewThree.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewThree.swift
+//  BlockLayout
+//
+//  Created by Tomás Elosua Cajigas on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewThree: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewThree()
+}
