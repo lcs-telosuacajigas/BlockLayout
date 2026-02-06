cf735ef4231d57c48190ca5c41c735bd1eb8adc6
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-02-06 09:49:55 -0500

    Completed block layout six.

---
+++ BlockLayout/ContentViewSix.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewSix.swift
+//  BlockLayout
+//
+//  Created by Tomás Elosua Cajigas on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewSix: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewSix()
+}
