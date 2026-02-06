1d4d186b508e0fc12b82f3e0d66c89374d93ea6a
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-02-06 12:20:44 -0500

    Block layout ten completed.

---
+++ BlockLayout/ContentViewTen.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewTen.swift
+//  BlockLayout
+//
+//  Created by Tomás Elosua Cajigas on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewTen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewTen()
+}
