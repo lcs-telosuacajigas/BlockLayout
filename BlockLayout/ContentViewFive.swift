09c4866743a4ec8cabd151222a931eb905e27788
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-02-06 09:46:48 -0500

    Completed block layout five.

---
+++ BlockLayout/ContentViewFive.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewFive.swift
+//  BlockLayout
+//
+//  Created by Tomás Elosua Cajigas on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewFive: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewFive()
+}
