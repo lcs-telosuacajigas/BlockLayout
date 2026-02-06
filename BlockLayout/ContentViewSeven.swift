54b1491ddd5a972abc6c9d3ea98c9a382ec7b15d
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-02-06 09:54:48 -0500

    Completed block layout seven.

---
+++ BlockLayout/ContentViewSeven.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewSeven.swift
+//  BlockLayout
+//
+//  Created by Tomás Elosua Cajigas on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewSeven: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewSeven()
+}
