7efeb2bf224a7ab5622c8ed21391575ee5923bc0
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-02-06 09:39:39 -0500

    Completed block layout four.

---
+++ BlockLayout/ContentViewFour.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewFour.swift
+//  BlockLayout
+//
+//  Created by Tomás Elosua Cajigas on 2026-02-06.
+//
+
+import SwiftUI
+
+struct ContentViewFour: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewFour()
+}
